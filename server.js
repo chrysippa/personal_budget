4d07210e080e41bef87ca4b1c236cb6516e180bb
Cassandra Blair <65232461+chrysippa@users.noreply.github.com>
2021-05-17 01:14:15 -0500

    initial commit with GET / and POST /envelopes

---
+++ server.js
@@ -0,0 +1,46 @@
+// model envelope
+// {id: 0, name: "", limit: 0}
+
+// TODO: add error handling, move helper funcs to other file
+
+const express = require('express'); 
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+let idIncrementor = 0;
+const generateId = () => {
+    return idIncrementor++;
+};
+
+const getEnvelopeById = (id) => {
+    const foundEnvelope = envelopes.find((e) => {e.id === id});
+    if (foundEnvelope) {
+        z
+    } else {
+        throw new Error("Envelope not found");
+    }
+};
+
+const envelopes = [];
+let totalFunds = 0;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.post('/envelopes', (req, res) => {
+    // should be sent {name: "", limit: 0}
+    const newEnvelope = req.body();
+    const newId = generateId();
+    newEnvelope.id = newId;
+    envelopes.push(newEnvelope);
+    res.status(201).send(getEnvelopeById(newId));
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
